af18d047014414236d65c81ca97363b7fcca204a
Luxas <135403604+Spl0itable@users.noreply.github.com>
2026-03-10 21:50:05 +1030

    v3.45.160

--- js/app.js
+++ js/app.js
@@ -22766,7 +22766,7 @@ function initWallpaperUI() {
 function showAbout() {
     const connectedRelays = nym.relayPool.size;
     nym.displaySystemMessage(`
-═══ Nymchat v3.45.159 ═══<br/>
+═══ Nymchat v3.45.160 ═══<br/>
 Protocol: <a href="https://nostr.com" target="_blank" rel="noopener" style="color: var(--secondary)">Nostr</a> (kind 20000 geohash channels)<br/>
 Connected Relays: ${connectedRelays} relays<br/>
 Your nym: ${nym.nym || 'Not set'}<br/>
