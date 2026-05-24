c5209f42c583bf3845d6460644ed7a6247e9efa1
Luxas <135403604+Spl0itable@users.noreply.github.com>
2026-05-24 10:25:46 +0930

    Revert "v3.66.399"
    This reverts commit 80a5a75d8248db7923cea56b0f74fcb8eb0a847a.

--- js/app.js
+++ js/app.js
@@ -3523,7 +3523,7 @@ function initWallpaperUI() {
     }
 }
 
-const NYMCHAT_VERSION = 'v3.66.399';
+const NYMCHAT_VERSION = 'v3.66.398';
 
 function showAbout() {
     const modal = document.getElementById('aboutModal');
