7121058a292b9ab2d71e4669646a68e3106a7be9
Luxas <135403604+Spl0itable@users.noreply.github.com>
2026-05-24 10:24:22 +0930

    Reapply "v3.66.409"
    This reverts commit ba59040a0f71c26d79a869f277f8a8b22cdf70be.

--- js/modules/pms.js
+++ js/modules/pms.js
@@ -11,11 +11,9 @@ Object.assign(NYM.prototype, {
         }
         this._insertMessageSorted(list, msg);
         this._indexMessage(conversationKey, msg);
-        if (list.length > this.pmStorageLimit) {
-            const drop = list.length - this.pmStorageLimit;
-            for (let i = 0; i < drop; i++) this._unindexMessage(list[i]);
-            list.splice(0, drop);
-        }
+        // Drop anything over the per-conversation cap. _pruneStorageKey also
+        // sweeps cached DOM fragments and live nodes.
+        this._pruneStorageKey(conversationKey, this.pmMessages, this.pmStorageLimit, 0);
         return list;
     },
 
